5d514b4b8aa401102bd74c67315a45810fd14fef
Shinobu KAWANO <kawanoshinobu@gmail.com>
2013-06-30 15:01:09 +0900

    add redis

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,13 @@
  */
 
 exports.index = function(req, res){
+  var redis = require('redis-url').connect(process.env.REDISTOGO_URL);
+
+  redis.set('foo', 'bar');
+
+  redis.get('foo', function(err, value) {
+    console.log('foo is: ' + value);
+  });
+
   res.render('index', { title: 'kawanoshinobu API' });
 };
