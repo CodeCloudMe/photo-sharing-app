f7faece86fe5513718fb9585c3f91676ce0de920
Shinobu KAWANO <kawanoshinobu@gmail.com>
2013-06-30 20:13:05 +0900

    add redis

---
+++ README.md
@@ -0,0 +1,12 @@
+# kawanoshinobu API
+
+Just for demonstration.
+
+## Dependencies
+
+kawanoshinobu API uses the following technologies:
+
+- [Node.js](http://nodejs.org)
+- [Redis](http://redis.io)
+- [Heroku](https://en.gravatar.com/site/implement/)
+
--- web.js
+++ web.js
@@ -5,7 +5,6 @@
 
 var express = require('express')
   , routes = require('./routes')
-  , user = require('./routes/user')
   , http = require('http')
   , path = require('path');
 
@@ -39,8 +38,8 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
+
 app.get('/test', function(req, res){
-  redisClient.set("test", "Hello! Redis.");
   redisClient.get("test", function (err, value) {
     res.json({title: value});
   });
@@ -50,45 +49,3 @@ app.listen(app.get('port'), function() {
   console.log("Listening on " + app.get('port'));
 });
 
-// http.createServer(app).listen(app.get('port'), function(){
-//   console.log('Express server listening on port ' + app.get('port'));
-// });
-
-// var express = require("express")
-//   , routes = require('./routes')
-//   , path = require('path');
-
-// var redis = require('redis');
-
-// var redisClient, subscriber;
-// var port = process.env.PORT || 5000;
-
-// if (process.env.REDISTOGO_URL) {
-//   var rtg = require("url").parse(process.env.REDISTOGO_URL);
-//   redisClient = redis.createClient(rtg.port, rtg.hostname);
-//   subscriber = redis.createClient(rtg.port, rtg.hostname);
-//   redisClient.auth(rtg.auth.split(":")[1]);
-//   subscriber.auth(rtg.auth.split(":")[1]);
-// } else {
-//   redisClient = redis.createClient();
-//   subscriber = redis.createClient();
-// }
-
-// var app = express();
-
-// app.set('port', process.env.PORT || 3000);
-// app.set('views', __dirname + '/views');
-// app.set('view engine', 'jade');
-// app.use(express.favicon());
-// app.use(express.bodyParser());
-// app.use(express.methodOverride());
-// app.use(app.router);
-// app.use(express.static(path.join(__dirname, 'public')));
-// app.use(express.logger());
-
-// app.get('/', routes.index);
-
-// var port = process.env.PORT || 5000;
-// app.listen(port, function() {
-//   console.log("Listening on " + port);
-// });
